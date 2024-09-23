e3a954ad984c4320d2fda25c8b896cdf7cad801d
HazemMansour <mansahmed9@gmail.com>
2024-09-23 18:01:21 +0300

    activity model

---
+++ controllers/activityController.js
@@ -0,0 +1,1 @@
+const activity = require('../models/activity');
---
+++ models/activity.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const activitySchema = new Schema({
+    Name: {
+        type: String,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: Number,
+        required: true,
+    },
+    timeline: {
+        start: {
+            type: Date,
+            required: true,
+        },
+        end: {
+            type: Date,
+            required: true,
+        },
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true,
+    },
+    tags: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Tag',
+        },
+    ],
+    specialDiscount: {
+        type: Number,
+        default: 0,
+    },
+    isBookingOpen: {
+        type: Boolean,
+        default: true,
+    },
+}, {
+    timestamps: true,
+});
+
+module.exports = mongoose.model('Activity', activitySchema);
