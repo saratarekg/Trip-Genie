35099ef27f0494831a35a26783bca62a72904bf1
HazemMansour <mansahmed9@gmail.com>
2024-09-23 19:37:14 +0300

    Activity CRUD

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -1,1 +1,66 @@
-const activity = require('../models/activity');
+const Activity = require('../models/activity');
+
+// GET all activities
+const getAllActivities = async (req, res) => {
+    try {
+        const activities = await Activity.find();
+        res.status(200).json(activities);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+
+const getActivityById = async (req, res) => {
+    try {
+        const activity = await Activity.findById(req.params.id);
+        if (!activity) {
+            return res.status(404).json({ message: 'Activity not found' });
+        }
+        res.status(200).json(activity);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const createActivity = async (req, res) => {
+    const activity = new Activity(req.body);
+    try {
+        await activity.save();
+        res.status(201).json(activity);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+const deleteActivity = async (req, res) => {
+    try {
+        const activity = await Activity.findByIdAndDelete(req.params.id);
+        if (!activity) {
+            return res.status(404).json({ message: 'Activity not found' });
+        }
+        res.status(204).json();
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const updateActivity = async (req, res) => {
+    try {
+        const activity = await Activity.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!activity) {
+            return res.status(404).json({ message: 'Activity not found' });
+        }
+        res.status(200).json(activity);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+module.exports = {
+    getAllActivities,
+    getActivityById,
+    createActivity,
+    deleteActivity,
+    updateActivity,
+};
--- routes/activityRoutes.js
+++ routes/activityRoutes.js
@@ -3,6 +3,11 @@ const activityController = require('../controllers/activityController');
 
 const router = express.Router();
 
+router.post('/', activityController.createActivity);
+router.get('/', activityController.getAllActivities);
+router.get('/:id', activityController.getActivityById);
+router.put('/:id', activityController.updateActivity);
+router.delete('/:id', activityController.deleteActivity);
 
 
 module.exports = router;
