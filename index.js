d4c72131a2b0b94e27a3470a384552361b989fb5
amyy847 <160797617+amyy847@users.noreply.github.com>
2024-09-24 21:55:25 +0300

    tourGuideProfile

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const itineraryRoutes = require('./routes/itineraryRoutes');
 const touristItineraryRoutes = require('./routes/touristItineraryRoutes');
 const sellerRoutes = require("./routes/sellerRoutes"); 
 const activityRoutes = require("./routes/activityRoutes");
+const tourGuideRoutes = require("./routes/tourGuideRoutes");
 const cookieParser = require('cookie-parser');
 const {requireAuth} = require('./middlewares/authMiddleware');
 
@@ -40,6 +41,7 @@ app.use('/seller', requireAuth, sellerRoutes);
 app.use('/activity',requireAuth, activityRoutes);
 app.use('/category', requireAuth, museumRoutes);
 app.use('/museums',requireAuth, categoryRoutes);
+app.use('/tourGuide',requireAuth, tourGuideRoutes);
 
 app.get('/sam', requireAuth, (req, res) => {
   res.send('Hello From Sam');
