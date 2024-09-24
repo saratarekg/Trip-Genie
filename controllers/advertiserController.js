5d88d47cfa1606f703f4df28db627ed2f3ddfe5a
Aby293 <abdelrahmanelaby@gmail.com>
2024-09-24 21:50:22 +0300

    removed old sign up apis

--- controllers/advertiserController.js
+++ controllers/advertiserController.js
@@ -1,18 +1,5 @@
 const Advertiser = require('../models/advertiser');
 
-const advertiserSignup = (req, res) => {
-    const {username, email, password} = req.body;
-    const advertiser = new Advertiser({username, email, password});
-
-    advertiser.save()
-        .then((result) => {
-            res.status(201).json({ advertiser: result });
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(400).json({ message: err.message });
-        });
-}
 
 const deleteAdvertiserAccount = async (req, res) => {
     try {
@@ -47,4 +34,4 @@ const getAdvertiserByID = async (req, res) => {
     }
 };
 
-module.exports = {advertiserSignup, deleteAdvertiserAccount,getAllAdvertisers,getAdvertiserByID};
+module.exports = { deleteAdvertiserAccount,getAllAdvertisers,getAdvertiserByID};
--- controllers/tourGuideController.js
+++ controllers/tourGuideController.js
@@ -1,18 +1,5 @@
 const TourGuide = require('../models/tourGuide');
 
-const tourGuideSignup = (req, res) => {
-    const {username, email, password} = req.body;
-    const tourGuide = new TourGuide({username, email, password});
-
-    tourGuide.save()
-        .then((result) => {
-            res.status(201).json({ tourGuide: result });
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(400).json({ message: err.message });
-        });
-}
 
 const deleteTourGuideAccount = async (req, res) => {
     try {
@@ -45,4 +32,4 @@ const getTourGuideByID = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
-module.exports = {tourGuideSignup, deleteTourGuideAccount, getAllTourGuides, getTourGuideByID};
+module.exports = { deleteTourGuideAccount, getAllTourGuides, getTourGuideByID};
