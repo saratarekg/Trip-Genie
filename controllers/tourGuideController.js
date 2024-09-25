802796a1618f2153f7b41742983b79e01ef68726
Radwa-Ibrahim0 <137509740+Radwa-Ibrahim0@users.noreply.github.com>
2024-09-25 21:01:53 +0300

    tout guide controller minor changes

--- controllers/tourGuideController.js
+++ controllers/tourGuideController.js
@@ -21,19 +21,7 @@ const getTourGuideProfile = async (req, res) => {
     }
 };
 
-<<<<<<< HEAD
-const getTourGuideByID = async (req, res) => {
-    try {
-        const tourGuide = await TourGuide.findById(req.params.id);
-        if (!tourGuide) {
-            return res.status(404).json({ message: 'Tour Guide not found' });
-        }
-        res.status(200).json(tourGuide);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-};
-=======
+
 // const getTourGuideByID = async (req, res) => {
 //     try {
 //         const tourGuide = await TourGuide.findById(req.params.id);
@@ -45,7 +33,6 @@ const getTourGuideByID = async (req, res) => {
 //         res.status(500).json({ error: error.message });
 //     }
 // };
->>>>>>> f2421607ef30e65151801f7dc965a19db940a464
 
 const updateTourGuide = async (req, res) => {
     try {
@@ -65,7 +52,6 @@ const updateTourGuide = async (req, res) => {
     }
 };
 
-<<<<<<< HEAD
 const updateTourGuideProfile = async (req, res) => {
     try {
         const tourGuideId = res.locals.user_id;  // Get the current user's ID
@@ -85,33 +71,8 @@ const updateTourGuideProfile = async (req, res) => {
     }
 };
 
-module.exports = { 
-    deleteTourGuideAccount, 
-    getAllTourGuides, 
-    getTourGuideByID, 
-    updateTourGuide,
-    getTourGuideProfile,
-    updateTourGuideProfile
-=======
-
-// Function to get all itineraries for a specific tour guide
-const getItinerariesByTourGuide = async (req, res) => {
-    try {
-        const { tourGuideId } = req.params; // Assuming tourGuideId is passed in the request params
-        const itineraries = await Itinerary.findByTourGuide(tourGuideId);
-        if (!itineraries || itineraries.length === 0) {
-            return res.status(404).json({ message: 'No itineraries found for this tour guide.' });
-        }
-        res.status(200).json(itineraries);
-    } catch (error) {
-        res.status(500).json({ message: 'An error occurred', error });
-    }
-};
-
-
 module.exports = {  
     updateTourGuide,
     getTourGuideProfile,
-    getItinerariesByTourGuide
->>>>>>> f2421607ef30e65151801f7dc965a19db940a464
+    updateTourGuideProfile
 };
