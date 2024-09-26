2f7fa04ee865a0fea67cd2d5b6ef134316490e75
samalouty <128055267+samalouty@users.noreply.github.com>
2024-09-26 23:01:17 +0300

    View Upcoming Activities done
    needs CSS and voila

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,24 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-
-// pages and components
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import SellerList from './pages/SellerList';
+import Login from './pages/Login';
+import ActivityList from './components/ActivityList';
+
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
-        <div className='pages'>
+      <Navbar/> 
+      {/* {isLoginPage ? null : <Navbar />} */}
+      <div className='pages'>
           <Routes>
             <Route path='/' element={<Home />} />
-
+            <Route path='/seller' element={<SellerList />} />
+            <Route path='/activity' element={<ActivityList />} />
+            <Route path='/login' element={<Login />} />
           </Routes>
         </div>
       </Router>
