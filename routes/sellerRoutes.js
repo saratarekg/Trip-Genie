00a5c7b5a49d6296ef60994afff2a7e3072b911d
ahmedakls <123523255+ahmedakls@users.noreply.github.com>
2024-09-28 01:59:11 +0300

    task 84 done

--- routes/sellerRoutes.js
+++ routes/sellerRoutes.js
@@ -8,6 +8,8 @@ router.put('/', sellerController.updateSeller);
 router.get('/', sellerController.getSeller);
 router.get('/sort-products-rating', productController.sortProductsByRating);
 
+
+router.get('/products/filterbyprice', productController.filterProductsByPrice);
 router.get('/products', productController.getAllProducts);
 router.get('/products/:name', productController.getProductbyName);
 router.post('/products', productController.addProduct);
