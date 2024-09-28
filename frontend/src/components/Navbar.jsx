5de3a46f11ae110ec2c22f131d23de66901e2af5
samalouty <128055267+samalouty@users.noreply.github.com>
2024-09-28 18:26:38 +0300

    added the logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/images/text-logo.png";
+import logo from "../assets/images/trip-genie.png";
 import searchGlass from "../assets/images/searchGlass.svg";
 
 import "./Navbar.css";
