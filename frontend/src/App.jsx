9ceeabaab6612ecf1f74b37b9d078d57e14c5ba6
ahmedakls <123523255+ahmedakls@users.noreply.github.com>
2024-10-04 17:55:58 +0300

    AdminDash_frontend
    initial frontend admin page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,41 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar1 from './components/Navbar1.jsx';
-import Home from './pages/Home.jsx';
-import Login from './pages/login.jsx';
-import SellerList from './components/SellerList.jsx';
-import ActivityList from './components/ActivityListAdvertiser.jsx';
-import ItineraryList from './components/ItineraryListTourGuide.jsx';
-import HistoricalPlaceList from './components/HistoricalPlaceListGovernor.jsx';
-import Tghome from './components/TourGuideHome.jsx';
-import Hero from './components/Hero.jsx';
-import { NavbarComponent } from './components/navbar.jsx';
-import { FooterComponent } from './components/footer.jsx';
-import { AllItinerariesComponent } from './components/all-trip-plans.jsx';
-import { SignupForm } from './components/signup-form.jsx';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar1 from "./components/Navbar1.jsx";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/login.jsx";
+import SellerList from "./components/SellerList.jsx";
+import ActivityList from "./components/ActivityListAdvertiser.jsx";
+import ItineraryList from "./components/ItineraryListTourGuide.jsx";
+import HistoricalPlaceList from "./components/HistoricalPlaceListGovernor.jsx";
+import Tghome from "./components/TourGuideHome.jsx";
+import Hero from "./components/Hero.jsx";
+import { NavbarComponent } from "./components/navbar.jsx";
+import { FooterComponent } from "./components/footer.jsx";
+import { AllItinerariesComponent } from "./components/all-trip-plans.jsx";
+import { SignupForm } from "./components/signup-form.jsx";
+import {AdminDash} from "./pages/AdminDash.jsx";
 function App() {
   return (
     <div className="App">
       <Router>
-      <NavbarComponent/> 
+        <NavbarComponent />
 
-
-      {/* {isLoginPage ? null : <Navbar />} */}
-      <div className='pages'>
+        {/* {isLoginPage ? null : <Navbar />} */}
+        <div className="pages">
           <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path="/" element={<Home />} />
             {/* <Route path='/tour-guide-home' element={<Tghome />} /> */}
-            <Route path='/login' element={<Login />} />
-            <Route path='/seller' element={<SellerList />} />
-            <Route path = '/activity' element = {<ActivityList/>}/>
-            <Route path = '/all-itineraries' element = {<AllItinerariesComponent/>}/>
-            <Route path='/sign-up' element={<SignupForm />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/seller" element={<SellerList />} />
+            <Route path="/activity" element={<ActivityList />} />
+            <Route path="/all-itineraries" element={<AllItinerariesComponent />}/>
+            <Route path="/sign-up" element={<SignupForm />} />
+            <Route path="/admin" element={<AdminDash />} />
             {/* <Route path = '/museums' element = {<HistoricalPlaceList/>}/> */}
           </Routes>
         </div>
       </Router>
-      <FooterComponent/>
+      <FooterComponent />
     </div>
   );
 }
