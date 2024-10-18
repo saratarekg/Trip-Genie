46fe0a73c88d65fcf6126218a532e113f9dd3182
amyy847 <160797617+amyy847@users.noreply.github.com>
2024-10-18 15:54:58 +0300

    tourist history

--- frontend/src/pages/TouristActivities.jsx
+++ frontend/src/pages/TouristActivities.jsx
@@ -76,7 +76,7 @@ export default function TouristActivities() {
   )
 
   return (
-    <div className="container mx-auto px-4 py-8 pt-20">
+    <div >
       <h1 className="text-3xl font-bold mb-8">My Activities and Itineraries</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         <div className="md:col-span-1">
