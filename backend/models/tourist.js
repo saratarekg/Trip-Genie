3202e4173d7a256bb1e88dde1f960248ccaaa922
zeinafadel <zeinamohamed411@gmail.com>
2024-11-03 16:39:03 +0200

    add card in settings

--- backend/models/tourist.js
+++ backend/models/tourist.js
@@ -97,6 +97,61 @@ const touristSchema = new Schema(
         },
       },
     ],
+    paymentMethod: {
+      type: String,
+      enum: ['CREDIT_CARD', 'DEBIT_CARD', 'WALLET'],
+      required: true,
+    },
+    cards: [
+      {
+        cardNumber: {
+          type: String,
+          required: function() {
+            return this.paymentMethod === 'CREDIT_CARD' || this.paymentMethod === 'DEBIT_CARD';
+          },
+          match: [/^[0-9]{16}$/, "Please enter a valid 16-digit card number"],
+        },
+        expiryDate: {
+          type: String,
+          required: function () {
+            return this.paymentMethod === 'CREDIT_CARD' || this.paymentMethod === 'DEBIT_CARD';
+          },
+          match: [/^(0[1-9]|1[0-2])\/[0-9]{2}$/, "Please enter a valid expiry date in MM/YY format"],
+          validate: {
+            validator: function (v) {
+              // Check if the expiry date is in the future
+              const [month, year] = v.split('/');
+              const expiryDate = new Date(`20${year}`, month - 1); // Create a date object
+              const currentDate = new Date(); // Get current date
+      
+              // Set the current date to the start of the month
+              currentDate.setDate(1);
+      
+              return expiryDate > currentDate; // Validate that expiry date is in the future
+            },
+            message: "Expiry date must be in the future",
+          },
+        },
+        holderName: {
+          type: String,
+          required: function() {
+            return this.paymentMethod === 'CREDIT_CARD' || this.paymentMethod === 'DEBIT_CARD';
+          },
+          trim: true,
+        },
+        cvv: {
+          type: String,
+          required: function() {
+            return this.paymentMethod === 'CREDIT_CARD' || this.paymentMethod === 'DEBIT_CARD';
+          },
+          match: [/^[0-9]{3,4}$/, "Please enter a valid 3 or 4 digit CVV"],
+        },
+        default: {
+          type: Boolean,
+          default: false,
+        },
+      },
+    ],
     preference: {
       budget: {  // max price
         type: Number,
