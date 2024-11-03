7f95479f7d011f26dd9dc96e9f5a2dd9fad7af80
amyy847 <160797617+amyy847@users.noreply.github.com>
2024-11-03 19:25:00 +0200

    currency in cart + wishlist

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -264,7 +264,7 @@ const editProductOfSeller = async (req, res) => {
 const getProductById = async (req, res) => {
   const { id } = req.params;
   try {
-    const product = await Product.findById(id).populate("seller").exec();
+    const product = await Product.findById(id).populate("seller").populate("currency").exec();
     if (!product) {
       return res.status(404).json({ message: "Product not found" });
     }
