a85d10f3464c05fe477a260308e76223ed7233e3
amyy847 <160797617+amyy847@users.noreply.github.com>
2024-11-04 22:17:02 +0200

    comments

--- frontend/src/components/ProductDetail.jsx
+++ frontend/src/components/ProductDetail.jsx
@@ -851,7 +851,11 @@ const ProductDetail = () => {
   }
 
   return (
-    <div className="min-h-screen bg-gray-100 pt-10">
+    <div className="min-h-screen bg-gray-100 ">
+      <div className="w-full bg-[#1A3B47] py-8 top-0 z-10">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        </div>
+      </div>
       <div className="container mx-auto px-4 py-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           <div className="md:col-span-2">
