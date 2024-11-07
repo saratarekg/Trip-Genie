b9c474ab7e187c28af5acb0bebfc5ba0b3df9a62
amyy847 <160797617+amyy847@users.noreply.github.com>
2024-11-08 01:36:52 +0200

    baby camera

--- frontend/src/components/touristProfile.jsx
+++ frontend/src/components/touristProfile.jsx
@@ -12,6 +12,8 @@ import {
   GraduationCap,
   Award,
   Star,
+  BriefcaseBusiness,
+  Camera,
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import PhoneInput from "react-phone-input-2";
@@ -349,6 +351,16 @@ export function TouristProfileComponent() {
                 </div>
               </Modal>
             </div>
+
+            {isEditing && (
+              <div className="absolute bottom-0 right-0 bg-white p-1 rounded-full shadow-md">
+                <Camera className="w-5 h-5 text-gray-600" 
+                 style={{ cursor: "pointer" }}
+                 onClick={toggleDropdown}/>
+              </div>
+            )}
+          
+            
             {isDropdownOpen && (
               <div className="absolute top-full mt-2 left-0 bg-white border border-gray-200 rounded-lg shadow-lg z-10 w-32">
                 <ul className="py-2">
@@ -532,7 +544,7 @@ export function TouristProfileComponent() {
 
           <div className="flex flex-col">
             <div className="flex items-center gap-2">
-              <Mail className="w-4 h-4 text-gray-500" />
+              <BriefcaseBusiness className="w-4 h-4 text-gray-500" />
               {isEditing ? (
                 <Input
                   type="jobOrStudent"
