16d177521a4413a21e916e40f6da0467096fa548
Houba3 <161231255+Houba3@users.noreply.github.com>
2024-12-09 00:19:41 +0200

    kamel ya aby

--- frontend/src/pages/TouristAttended.jsx
+++ frontend/src/pages/TouristAttended.jsx
@@ -12,6 +12,8 @@ import {
   DialogContent,
   DialogHeader,
   DialogTitle,
+  DialogDescription,
+  DialogFooter,
 } from "@/components/ui/dialog";
 import { Label } from "@/components/ui/label";
 import { useToast } from "@/hooks/use-toast";
@@ -96,83 +98,83 @@ const ActivityCard = ({ booking, onSelect, userInfo, exchangeRates }) => {
 const SkeletonCard = () => {
   return (
     <Card className="group relative flex items-center gap-4 p-2 transition-all hover:shadow-lg cursor-pointer">
-  <div className="relative h-36 w-36 shrink-0 rounded-sm bg-gray-300 animate-pulse" />
-  <div className="flex flex-1 flex-col gap-2"> {/* Reduced gap between elements */}
-    <div className="flex items-start justify-between">
-      <div className="w-3/4 h-6 bg-gray-300 rounded-sm animate-pulse mr-2" /> {/* Increased width for title */}
-      <div className="w-1/3 h-6 bg-gray-300 rounded-sm animate-pulse mr-2" /> {/* Increased width for other small section */}
-    </div>
+      <div className="relative h-36 w-36 shrink-0 rounded-sm bg-gray-300 animate-pulse" />
+      <div className="flex flex-1 flex-col gap-2"> {/* Reduced gap between elements */}
+        <div className="flex items-start justify-between">
+          <div className="w-3/4 h-6 bg-gray-300 rounded-sm animate-pulse mr-2" /> {/* Increased width for title */}
+          <div className="w-1/3 h-6 bg-gray-300 rounded-sm animate-pulse mr-2" /> {/* Increased width for other small section */}
+        </div>
 
-    <div className="flex items-center gap-2 text-sm text-[#5D9297]"> 
-  {/* Increased gap between elements */}
-  <div className="flex items-center gap-2">
-    <div className="h-4 w-4 p-2 bg-gray-300 rounded-full animate-pulse" />
-    <div className="w-4/5 pl-11 pr-11 pt-1 pb-1  h-5 bg-gray-300 rounded-sm animate-pulse" /> {/* Increased width */}
-  </div>
-  <div className="flex items-center gap-2">
-    <div className="h-4 w-4 p-2 bg-gray-300 rounded-full animate-pulse" />
-    <div className="w-4/5 h-5 pl-11 pr-11 pt-1 pb-1 bg-gray-300 rounded-sm animate-pulse " /> {/* Increased width */}
-  </div>
-</div>
+        <div className="flex items-center gap-2 text-sm text-[#5D9297]">
+          {/* Increased gap between elements */}
+          <div className="flex items-center gap-2">
+            <div className="h-4 w-4 p-2 bg-gray-300 rounded-full animate-pulse" />
+            <div className="w-4/5 pl-11 pr-11 pt-1 pb-1  h-5 bg-gray-300 rounded-sm animate-pulse" /> {/* Increased width */}
+          </div>
+          <div className="flex items-center gap-2">
+            <div className="h-4 w-4 p-2 bg-gray-300 rounded-full animate-pulse" />
+            <div className="w-4/5 h-5 pl-11 pr-11 pt-1 pb-1 bg-gray-300 rounded-sm animate-pulse " /> {/* Increased width */}
+          </div>
+        </div>
 
 
 
-    <div className="mt-2 flex items-center justify-between"> {/* Adjusted margin-top */}
-      <div className="w-3/4 h-6 bg-gray-300 rounded-sm mr-2 animate-pulse" /> {/* Increased width */}
-      <div className="flex gap-2">
-        <Button
-          size="sm"
-          variant="default"
-          className="bg-[#388A94] text-[#388A94] hover:bg-[#2e6b77]"
-          disabled
-        >
-          View Details
-        </Button>
+        <div className="mt-2 flex items-center justify-between"> {/* Adjusted margin-top */}
+          <div className="w-3/4 h-6 bg-gray-300 rounded-sm mr-2 animate-pulse" /> {/* Increased width */}
+          <div className="flex gap-2">
+            <Button
+              size="sm"
+              variant="default"
+              className="bg-[#388A94] text-[#388A94] hover:bg-[#2e6b77]"
+              disabled
+            >
+              View Details
+            </Button>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</Card>
+    </Card>
 
   );
 };
 const SkeletonDialog = () => {
   return (
     <Dialog>
-    <DialogContent className="sm:max-w-[400px] bg-white rounded-lg shadow-lg p-4">
-      <DialogHeader>
-        <DialogTitle className="text-lg font-semibold text-[#1A3B47]">
-          <div className="w-2/3 h-6 bg-gray-300 rounded-sm animate-pulse" />
-          <div className="w-1/2 h-8 bg-gray-300 rounded-sm animate-pulse mt-2" />
-        </DialogTitle>
-      </DialogHeader>
+      <DialogContent className="sm:max-w-[400px] bg-white rounded-lg shadow-lg p-4">
+        <DialogHeader>
+          <DialogTitle className="text-lg font-semibold text-[#1A3B47]">
+            <div className="w-2/3 h-6 bg-gray-300 rounded-sm animate-pulse" />
+            <div className="w-1/2 h-8 bg-gray-300 rounded-sm animate-pulse mt-2" />
+          </DialogTitle>
+        </DialogHeader>
 
-      <div className="border-t border-gray-200 pt-4">
-        {/* Name and Booking ID */}
-        <div className="flex justify-between items-center mb-3">
+        <div className="border-t border-gray-200 pt-4">
+          {/* Name and Booking ID */}
+          <div className="flex justify-between items-center mb-3">
+            <div className="w-2/3 h-4 bg-gray-300 rounded-sm animate-pulse" />
+            <div className="w-1/4 h-4 bg-gray-300 rounded-sm animate-pulse" />
+          </div>
+
+          {/* Date & Time and Paid Via */}
+          <div className="flex justify-between items-center mb-3">
+            <div className="w-2/3 h-4 bg-gray-300 rounded-sm animate-pulse" />
+            <div className="w-1/3 h-4 bg-gray-300 rounded-sm animate-pulse" />
+          </div>
+
+          {/* Tickets and Total Price */}
+          <div className="flex items-center justify-between border-t border-gray-200 mt-4 pt-4">
+            <div className="w-1/4 h-6 bg-gray-300 rounded-sm animate-pulse" />
+            <div className="w-1/4 h-6 bg-gray-300 rounded-sm animate-pulse" />
+          </div>
+        </div>
+
+        <div className="border-t border-gray-200 pt-4">
+          {/* Thank You Message */}
+          <div className="w-1/2 h-4 bg-gray-300 rounded-sm animate-pulse mb-4" />
+          {/* Reminder and Info */}
           <div className="w-2/3 h-4 bg-gray-300 rounded-sm animate-pulse" />
-          <div className="w-1/4 h-4 bg-gray-300 rounded-sm animate-pulse" />
         </div>
-
-        {/* Date & Time and Paid Via */}
-        <div className="flex justify-between items-center mb-3">
-          <div className="w-2/3 h-4 bg-gray-300 rounded-sm animate-pulse" />
-          <div className="w-1/3 h-4 bg-gray-300 rounded-sm animate-pulse" />
-        </div>
-
-        {/* Tickets and Total Price */}
-        <div className="flex items-center justify-between border-t border-gray-200 mt-4 pt-4">
-          <div className="w-1/4 h-6 bg-gray-300 rounded-sm animate-pulse" />
-          <div className="w-1/4 h-6 bg-gray-300 rounded-sm animate-pulse" />
-        </div>
-      </div>
-
-      <div className="border-t border-gray-200 pt-4">
-        {/* Thank You Message */}
-        <div className="w-1/2 h-4 bg-gray-300 rounded-sm animate-pulse mb-4" />
-        {/* Reminder and Info */}
-        <div className="w-2/3 h-4 bg-gray-300 rounded-sm animate-pulse" />
-      </div>
-    </DialogContent>
+      </DialogContent>
     </Dialog>
   );
 };
@@ -204,9 +206,10 @@ export default function TouristAttendedActivities() {
   const [isViewDialogOpen, setIsViewDialogOpen] = useState(false);
   const [userInfo, setUserInfo] = useState(null);
   const [exchangeRates, setExchangeRates] = useState({});
-  const [tourist,setTourist] = useState(null);
+  const [tourist, setTourist] = useState(null);
   const navigate = useNavigate();
   const { toast } = useToast();
+  const [isReviewDialogOpen, setIsReviewDialogOpen] = useState(false);
 
   const fetchExchangeRates = useCallback(async () => {
     try {
@@ -273,6 +276,26 @@ export default function TouristAttendedActivities() {
     fetchAllData();
   }, [fetchUserInfo, fetchExchangeRates]);
 
+  const handleReviewClick = async (id) => {
+    try {// fetch the activity from the backend
+      const response = await axios.get(`http://localhost:4000/tourist/activities/${id}`, {
+        headers: { Authorization: `Bearer ${Cookies.get("jwt")}` }
+      }
+      )
+
+
+      if (response.status === 200) {
+        navigate(`/activity/${id}`);
+      } else {
+        setIsReviewDialogOpen(true);
+      }
+    }
+    catch (error) {
+      console.error("Error fetching activity:", error);
+      setIsReviewDialogOpen(true);
+    }
+  };
+
   const handleActivitySelect = (booking) => {
     setSelectedBooking(booking);
     setIsViewDialogOpen(true);
@@ -302,7 +325,7 @@ export default function TouristAttendedActivities() {
               <SkeletonCard key={idx} />
             ))}
           </div>
-  
+
           {/* Skeleton for Dialog */}
           <SkeletonDialog />
         </div>
@@ -310,11 +333,11 @@ export default function TouristAttendedActivities() {
     );
   };
 
-if (error) return <div>{error}</div>;
+  if (error) return <div>{error}</div>;
 
   return (
     <div className="bg-gray-100 min-h-screen">
-              {/* <h1 className="text-3xl font-bold mb-2">Attended Activities</h1>
+      {/* <h1 className="text-3xl font-bold mb-2">Attended Activities</h1>
         <p className="text-sm text-gray-500 mb-2">Activities / Attended</p>
          */}
       <Toaster />
@@ -353,109 +376,131 @@ if (error) return <div>{error}</div>;
       </div>
 
       <Dialog open={isViewDialogOpen} onOpenChange={setIsViewDialogOpen}>
-  <DialogContent className="sm:max-w-[330px] bg-white rounded-lg shadow-lg p-4">
-    <DialogHeader>
-      <DialogTitle className="text-lg font-semibold text-[#1A3B47]">
-        <div>
-          Booking Receipt for
-        </div>
-        <div className="text-[#388A94] text-xl font-bold">
-          {selectedBooking?.activity.name}
-        </div>
-        <div className="text-sm text-gray-400 mt-1">
-          Booking ID: <span className=" font-semibold">
-            {selectedBooking?._id ? selectedBooking._id.substring(0, 10) : "AB123456"}
-          </span>
-        </div>
-      </DialogTitle>
-    </DialogHeader>
+        <DialogContent className="sm:max-w-[330px] bg-white rounded-lg shadow-lg p-4">
+          <DialogHeader>
+            <DialogTitle className="text-lg font-semibold text-[#1A3B47]">
+              <div>
+                Booking Receipt for
+              </div>
+              <div className="text-[#388A94] text-xl font-bold">
+                {selectedBooking?.activity.name}
+              </div>
+              <div className="text-sm text-gray-400 mt-1">
+                Booking ID: <span className=" font-semibold">
+                  {selectedBooking?._id ? selectedBooking._id.substring(0, 10) : "AB123456"}
+                </span>
+              </div>
+            </DialogTitle>
+          </DialogHeader>
 
-    {/* Main content */}
-    <div className="border-t border-gray-200 pt-4 space-y-4">
-      {/* Date */}
-      <div className="flex justify-between items-center">
-        <p className="text-sm font-medium text-gray-400">Date</p>
-        <p className="text-[#1A3B47] font-semibold text-right">
-          {new Date(selectedBooking?.activity.timing).toLocaleDateString(
-            "en-US",
-            { year: "numeric", month: "long", day: "numeric" }
-          )}
-        </p>
-      </div>
 
-      {/* Time */}
-      <div className="flex justify-between items-center">
-        <p className="text-sm font-medium text-gray-400">Time</p>
-        <p className="text-[#1A3B47] font-semibold text-right">
-          {new Date(selectedBooking?.activity.timing).toLocaleTimeString(
-            "en-US",
-            { hour: "2-digit", minute: "2-digit" }
-          )}
-        </p>
-      </div>
 
-      {/* Tourist Information */}
-      <div className="flex justify-between items-center">
-        <p className="text-sm font-medium text-gray-400">Recipient</p>
-        <div className="text-right">
-          <p className="text-[#1A3B47] font-semibold">
-            {tourist?.fname || "John"} {tourist?.lname || "Doe"}
-          </p>
-          <p className="text-[#1A3B47] text-xs">
-            {tourist?.email || "john.doe@example.com"}
-          </p>
-        </div>
-      </div>
+          {/* Main content */}
+          <div className="border-t border-gray-200 pt-4 space-y-4">
+            {/* Date */}
+            <div className="flex justify-between items-center">
+              <p className="text-sm font-medium text-gray-400">Date</p>
+              <p className="text-[#1A3B47] font-semibold text-right">
+                {new Date(selectedBooking?.activity.timing).toLocaleDateString(
+                  "en-US",
+                  { year: "numeric", month: "long", day: "numeric" }
+                )}
+              </p>
+            </div>
 
-      {/* Tickets Booked */}
-      <div className="flex justify-between items-center">
-        <p className="text-sm font-medium text-gray-400">Tickets Booked</p>
-        <p className="text-[#1A3B47] font-semibold text-right">
-          {selectedBooking?.numberOfTickets || "1"} Tickets
-        </p>
-      </div>
+            {/* Time */}
+            <div className="flex justify-between items-center">
+              <p className="text-sm font-medium text-gray-400">Time</p>
+              <p className="text-[#1A3B47] font-semibold text-right">
+                {new Date(selectedBooking?.activity.timing).toLocaleTimeString(
+                  "en-US",
+                  { hour: "2-digit", minute: "2-digit" }
+                )}
+              </p>
+            </div>
 
-      {/* Total Price */}
-      <div className="flex justify-between items-center border-t border-gray-200 pt-4">
-        <p className="text-xl font-semibold text-gray-600">Total Price</p>
-        <p className="text-2xl font-bold text-[#388A94]">
-          {getFormattedPrice(selectedBooking?.paymentAmount || 0)}
-        </p>
-      </div>
+            {/* Tourist Information */}
+            <div className="flex justify-between items-center">
+              <p className="text-sm font-medium text-gray-400">Recipient</p>
+              <div className="text-right">
+                <p className="text-[#1A3B47] font-semibold">
+                  {tourist?.fname || "John"} {tourist?.lname || "Doe"}
+                </p>
+                <p className="text-[#1A3B47] text-xs">
+                  {tourist?.email || "john.doe@example.com"}
+                </p>
+              </div>
+            </div>
 
-      {/* Payment Method */}
-      <div className="flex justify-between items-center">
-        <p className="text-sm text-gray-400">Payment Method</p>
-        <p className="text-[#1A3B47] font-semibold text-right">
-          via {selectedBooking?.paymentType || "Credit Card"}
-        </p>
-      </div>
-    </div>
+            {/* Tickets Booked */}
+            <div className="flex justify-between items-center">
+              <p className="text-sm font-medium text-gray-400">Tickets Booked</p>
+              <p className="text-[#1A3B47] font-semibold text-right">
+                {selectedBooking?.numberOfTickets || "1"} Tickets
+              </p>
+            </div>
 
-    {/* Footer */}
-    <div className="border-t border-gray-200 pt-4 text-center">
-      <p className="text-sm text-[#1A3B47] font-medium">
-        Thank you for joining the activity!
-      </p>
-      <div className="flex items-center text-xs text-gray-500 mt-2">
-        <Info className="h-6 w-6 text-gray-400 mr-1 mb-3" />
-        <span>
-              We'd love to hear your thoughts!{" "}
-              <span 
-                className="text-[#388A94] cursor-pointer hover:underline"
-                onClick={() => {
-                  setIsViewDialogOpen(false);
-                  navigate(`/activity/${selectedBooking?.activity._id}`);
-                }}
-              >
-                Please leave a review
-              </span>{" "}
-              to help us improve.
-            </span>
-      </div>
-    </div>
-  </DialogContent>
-</Dialog>
+            {/* Total Price */}
+            <div className="flex justify-between items-center border-t border-gray-200 pt-4">
+              <p className="text-xl font-semibold text-gray-600">Total Price</p>
+              <p className="text-2xl font-bold text-[#388A94]">
+                {getFormattedPrice(selectedBooking?.paymentAmount || 0)}
+              </p>
+            </div>
+
+            {/* Payment Method */}
+            <div className="flex justify-between items-center">
+              <p className="text-sm text-gray-400">Payment Method</p>
+              <p className="text-[#1A3B47] font-semibold text-right">
+                via {selectedBooking?.paymentType || "Credit Card"}
+              </p>
+            </div>
+          </div>
+
+          {/* Footer */}
+          <div className="border-t border-gray-200 pt-4 text-center">
+            <p className="text-sm text-[#1A3B47] font-medium">
+              Thank you for joining the activity!
+            </p>
+            <div className="flex items-center text-xs text-gray-500 mt-2">
+              <Info className="h-6 w-6 text-gray-400 mr-1 mb-3" />
+              <span>
+                We'd love to hear your thoughts!{" "}
+                <span
+                  className="text-[#388A94] cursor-pointer hover:underline"
+                  onClick={() => {
+                    setIsViewDialogOpen(false);
+                    handleReviewClick(selectedBooking?.activity._id);
+                  }}
+                >
+                  Please leave a review
+                </span>{" "}
+                to help us improve.
+              </span>
+            </div>
+          </div>
+        </DialogContent>
+      </Dialog>
+      <Dialog open={isReviewDialogOpen} onOpenChange={setIsReviewDialogOpen}>
+        <DialogContent className="sm:max-w-[330px] bg-white rounded-lg shadow-lg p-4">
+          <DialogHeader>
+            <DialogTitle>Activity Deleted</DialogTitle>
+            <DialogDescription>
+              The activity you are trying to review has been deleted and is no longer available.
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter className="flex justify-end">
+            <Button
+              onClick={() => setIsReviewDialogOpen(false)}
+              className="bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded-lg"
+            >
+              Close
+            </Button>
+          </DialogFooter>
+
+
+        </DialogContent>
+      </Dialog>
 
 
     </div>
