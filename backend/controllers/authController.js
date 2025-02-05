f6773cdafc54d82cdea22469b17316f9f1ccc2bb
Aby293 <abdelrahmanelaby@gmail.com>
2025-02-05 15:11:58 +0200

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -130,14 +130,14 @@ const login = async (req, res) => {
 
     const token = createToken(user._id, role);
     res.cookie("jwt", token, {
-      httpOnly: true, // Prevent client-side JavaScript from accessing the cookie
+      httpOnly: false, // Prevent client-side JavaScript from accessing the cookie
       secure: process.env.NODE_ENV === "production", // Use `secure` only in production (HTTPS)
       sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // Allow cross-origin cookies in production
       maxAge: process.env.MAX_AGE * 1000, // Ensure `maxAge` is set in milliseconds
     });
 
     res.cookie("role", role, {
-      httpOnly: true, // Prevent client-side JavaScript from accessing the cookie
+      httpOnly: false, // Prevent client-side JavaScript from accessing the cookie
       secure: process.env.NODE_ENV === "production", // Use `secure` only in production (HTTPS)
       sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // Allow cross-origin cookies in production
       maxAge: process.env.MAX_AGE * 1000, // Ensure `maxAge` is set in milliseconds
